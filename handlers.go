dd05607a4869a54524a1eaf08a1de56753443f37
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-02 04:54:30 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -9,17 +9,9 @@ import (
 	"github.com/yanzay/tbot/v2"
 )
 
-// Handle the /start command here
-func (a *application) startHandler(m *tbot.Message) {
-	msg := "hello"
-	a.client.SendMessage(m.Chat.ID, msg, tbot.OptParseModeMarkdown)
-}
-
-// Handle the msg command here
-func (a *application) msgHandler(m *tbot.Message) {
 	//deer by asciiart.eu
 	//flower by eng50232@leonis.nus.sg
-	magicDeer := `
+	var magicDeer = `
  \ /   .              *
 * :       ))    ((
    \     // (") \\   '      .
@@ -31,6 +23,48 @@ o       /  /     ,|
         \'~~    | |   *
   .      |      | |
 		`
+
+// Handle the /start command here
+func (a *application) startHandler(m *tbot.Message) {
+	msg := "hello"
+	a.client.SendMessage(m.Chat.ID, msg, tbot.OptParseModeMarkdown)
+}
+
+// Handle the msg command here
+func (a *application) msgHandler(m *tbot.Message) {
+	msg := "Ты сделал что-то не так"
+	if signs[m.Text] != ""{
+	signs := map[string]string{
+		"Овен":     "aries",
+		"Телец":    "taurus",
+		"Близнецы": "gemini",
+		"Рак":      "cancer",
+		"Лев":      "leo",
+		"Дева":     "virgio",
+		"Весы":     "libra",
+		"Скорпион": "scorpio",
+		"Стрелец":  "saggitarius",
+		"Козерог":  "capricorn",
+		"Водолей":  "aquarius",
+		"Рыбы":     "pisces"}
+	day := "tod"
+	res, err := http.Get("https://ignio.com/r/daily/" + day + "/" + signs[m.Text] + ".html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(`div[style="margin: 20px 0;"]`).Each(func(i int, s *goquery.Selection) {
+		msg = fmt.Sprintf("Ежедневний гороскоп для тебя, " + signs[m.Text] + ": \n" + strings.TrimSpace(s.Text()))
+	})}else{	
 	answer := map[int]string{
 		0: "Да",
 		1: "Нет",
@@ -42,7 +76,6 @@ o       /  /     ,|
 		7: "100%",
 		8: "1 из 100",
 		9: "Попробуй ещё раз"}
-	msg := "Ты сделал что-то не так!"
 	rand.Seed(time.Now().UnixNano())
 	arr := strings.Split(m.Text, " или ")
 	if len(arr) > 1 {
@@ -58,7 +91,7 @@ o       /  /     ,|
 		rnd := rand.Intn(10)
 		msg = answer[rnd]
 	}
-	msg = fmt.Sprintf("```\n< %s > %s```", msg, magicDeer)
+		msg = fmt.Sprintf("```\n< %s > %s```", msg, magicDeer)}
 	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
 	tsleep := rand.Intn(2500-500) + 500
 	time.Sleep(time.Duration(tsleep) * time.Millisecond)
