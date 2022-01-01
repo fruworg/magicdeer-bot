dad08263f2959581b74c919852a13333471db39c
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-02 04:58:06 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strings"
 	"time"
-	"log"
-	"fmt"
 
-	"github.com/yanzay/tbot/v2"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/yanzay/tbot/v2"
 )
 
-	//deer by asciiart.eu
-	//flower by eng50232@leonis.nus.sg
-	var magicDeer = `
+//deer by asciiart.eu
+//flower by eng50232@leonis.nus.sg
+var magicDeer = `
  \ /   .              *
 * :       ))    ((
    \     // (") \\   '      .
@@ -36,65 +36,67 @@ func (a *application) startHandler(m *tbot.Message) {
 // Handle the msg command here
 func (a *application) msgHandler(m *tbot.Message) {
 	msg := "Ты сделал что-то не так"
-	if signs[m.Text] != ""{
-	signs := map[string]string{
-		"Овен":     "aries",
-		"Телец":    "taurus",
-		"Близнецы": "gemini",
-		"Рак":      "cancer",
-		"Лев":      "leo",
-		"Дева":     "virgio",
-		"Весы":     "libra",
-		"Скорпион": "scorpio",
-		"Стрелец":  "saggitarius",
-		"Козерог":  "capricorn",
-		"Водолей":  "aquarius",
-		"Рыбы":     "pisces"}
-	day := "tod"
-	res, err := http.Get("https://ignio.com/r/daily/" + day + "/" + signs[m.Text] + ".html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.Find(`div[style="margin: 20px 0;"]`).Each(func(i int, s *goquery.Selection) {
-		msg = fmt.Sprintf("Ежедневний гороскоп для тебя, %s: \n%s" signs[m.Text], strings.TrimSpace(s.Text()))
-	})}else{	
-	answer := map[int]string{
-		0: "Да",
-		1: "Нет",
-		2: "Это не важно",
-		3: "...",
-		4: "У тебя есть проблемы серьёзней",
-		5: "Да, хотя зря",
-		6: "Никогда",
-		7: "100%",
-		8: "1 из 100",
-		9: "Попробуй ещё раз"}
-	rand.Seed(time.Now().UnixNano())
-	arr := strings.Split(m.Text, " или ")
-	if len(arr) > 1 {
-		msg = "Ты не оставил мне выбора"
-		for i := 0; i < len(arr)-1; i++ {
-			if strings.TrimRight(arr[i], "?") != strings.TrimRight(arr[i+1], "?") {
-				rnd := rand.Intn(len(arr))
-				msg = strings.TrimRight(arr[rnd], "?")
-				continue
-			}
+	if signs[m.Text] != "" {
+		signs := map[string]string{
+			"Овен":     "aries",
+			"Телец":    "taurus",
+			"Близнецы": "gemini",
+			"Рак":      "cancer",
+			"Лев":      "leo",
+			"Дева":     "virgio",
+			"Весы":     "libra",
+			"Скорпион": "scorpio",
+			"Стрелец":  "saggitarius",
+			"Козерог":  "capricorn",
+			"Водолей":  "aquarius",
+			"Рыбы":     "pisces"}
+		day := "tod"
+		res, err := http.Get("https://ignio.com/r/daily/" + day + "/" + signs[m.Text] + ".html")
+		if err != nil {
+			log.Fatal(err)
 		}
+		defer res.Body.Close()
+		if res.StatusCode != 200 {
+			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		}
+
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		doc.Find(`div[style="margin: 20px 0;"]`).Each(func(i int, s *goquery.Selection) {
+			msg = fmt.Sprintf("Ежедневний гороскоп для тебя, %s: \n%s", signs[m.Text], strings.TrimSpace(s.Text()))
+		})
 	} else {
-		rnd := rand.Intn(10)
-		msg = answer[rnd]
+		answer := map[int]string{
+			0: "Да",
+			1: "Нет",
+			2: "Это не важно",
+			3: "...",
+			4: "У тебя есть проблемы серьёзней",
+			5: "Да, хотя зря",
+			6: "Никогда",
+			7: "100%",
+			8: "1 из 100",
+			9: "Попробуй ещё раз"}
+		rand.Seed(time.Now().UnixNano())
+		arr := strings.Split(m.Text, " или ")
+		if len(arr) > 1 {
+			msg = "Ты не оставил мне выбора"
+			for i := 0; i < len(arr)-1; i++ {
+				if strings.TrimRight(arr[i], "?") != strings.TrimRight(arr[i+1], "?") {
+					rnd := rand.Intn(len(arr))
+					msg = strings.TrimRight(arr[rnd], "?")
+					continue
+				}
+			}
+		} else {
+			rnd := rand.Intn(10)
+			msg = answer[rnd]
+		}
+		msg = fmt.Sprintf("```\n< %s > %s```", msg, magicDeer)
 	}
-		msg = fmt.Sprintf("```\n< %s > %s```", msg, magicDeer)}
 	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
 	tsleep := rand.Intn(2500-500) + 500
 	time.Sleep(time.Duration(tsleep) * time.Millisecond)
