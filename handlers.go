18b8a4a995d650a28c0a50ebc536537affa58d54
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-02 18:32:02 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -50,6 +50,26 @@ func (a *application) msgHandler(m *tbot.Message) {
 		"козерог":  "capricorn",
 		"водолей":  "aquarius",
 		"рыбы":     "pisces"}
+	
+	if m.Text == "тест"{
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	msg = fmt.Sprintf("%s; %s", name, weight)
+	}
+	
 	if signs[strings.ToLower(m.Text)] != "" {
 		day := "tod"
 		res, err := http.Get("https://ignio.com/r/daily/" + day + "/" + signs[strings.ToLower(m.Text)] + ".html")
