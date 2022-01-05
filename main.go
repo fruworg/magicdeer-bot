9bb56dbbd775fae1a79e92a2933a09a254b5b599
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-05 05:08:37 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	
 	"github.com/yanzay/tbot/v2"
 )
 
@@ -18,16 +17,8 @@ var (
 	token string
 )
 
-func init() {
-	e := godotenv.Load()
-	if e != nil {
-		log.Println(e)
-	}
-	token = os.Getenv("TELEGRAM_TOKEN")
-}
-
 func main() {
-	bot = tbot.New(token, tbot.WithWebhook("https://magicdeer-bot.herokuapp.com", ":"+os.Getenv("PORT")))
+	bot = tbot.New(os.Getenv("TELEGRAM_TOKEN"), tbot.WithWebhook("https://magicdeer-bot.herokuapp.com", ":"+os.Getenv("PORT")))
 	app.client = bot.Client()
 	bot.HandleMessage("/start", app.startHandler)
 	bot.HandleMessage(".+", app.msgHandler)
