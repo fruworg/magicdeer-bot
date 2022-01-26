8f6fc74d8dd75f4cbee17c8a2bffb1560bea490b
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-26 14:51:54 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -110,7 +110,10 @@ func (a *application) msgHandler(m *tbot.Message) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		
+		if m.Text == "/tomorrow"{
+		replacer := strings.NewReplacer("Сегодня", "Завтра", "сегодня", "завтра")
+		s.Text = replacer.Replace(s.Text)	
+		}
 		doc.Find(`div[style="margin: 20px 0;"]`).Each(func(i int, s *goquery.Selection) {
 			msg = fmt.Sprintf("Гороскоп %s, %s: \n%s", outday, sign, strings.TrimSpace(s.Text()))
 		})}
